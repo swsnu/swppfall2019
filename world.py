7783b85a0f4a7bc0e76f2590af899b26fc4e8065
qgusgh <hyunho1997@naver.com>
2019-09-16 18:00:25 +0900

    added world.py

---
+++ world.py
@@ -0,0 +1,13 @@
+# world.py file
+import time
+
+def hello_world():
+    print(“hello world”)
+
+def bye_world():
+    time.sleep(2)
+    print(“bye world”)
+
+if __name__ == “__main__”:
+    hello_world()
+    bye_world()
